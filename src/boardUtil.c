df4aee6ff8dc00023b405761b95341c856f0403e
fbarrylenoch <frankinbarry@gmail.com>
2014-12-02 22:57:28 -0600

    Massive Load

--- src/boardUtil.c
+++ src/boardUtil.c
@@ -1,6 +1,5 @@
 #include "TM4C123GH6PM.h"
 #include "boardUtil.h"
-#include "gpio.h"
 
 
 void DisableInterrupts(void)
@@ -14,6 +13,11 @@ void EnableInterrupts(void)
   __asm {
     CPSIE  I
   }
+}
+
+void f14_project_boardUtil(void){
+	
+	uart0_config_gpio();
 }
 
 //*****************************************************************************
@@ -35,4 +39,57 @@ void serialDebugInit(void)
   uart0_init_115K();
 }
 
+//*****************************************************************************
+//*****************************************************************************
+void pushButtonInit (void)
+{		
+		//****Configure push buttons*****
+		//*******************************
+		// Enable port F for push puttons
+		gpio_enable_port(GPIOF_BASE);
+		
+		// configure the pushbuttons as digital pins
+		gpio_config_digital_enable(GPIOF_BASE, PF1 | PF2 | PF3 | PF4);
+		
+		// Eanable push buttons up as input
+		gpio_config_enable_input(GPIOF_BASE, PF1 | PF2 | PF3 | PF4);
+		
+		// Enable the pushbuttons with internal pull-up resistors
+		gpio_config_enable_pullup(GPIOF_BASE, PF1 | PF2 | PF3 | PF4);
+	
+		//******Configure PS2 button*****
+		//*******************************
+				// Enable port F for push puttons
+		gpio_enable_port(GPIOE_BASE);
+		
+		// configure the pushbuttons as digital pins
+		gpio_config_digital_enable(GPIOE_BASE, PE0);
+		
+		// Eanable push buttons up as input
+		gpio_config_enable_input(GPIOE_BASE, PE0);
+		
+		// Enable the pushbuttons with internal pull-up resistors
+		gpio_config_enable_pullup(GPIOE_BASE, PE0);
+}
 
+//*****************************************************************************
+//*****************************************************************************
+void joyStickInit (void)
+{
+		// Eanble port E for the Joy-Stick
+		gpio_enable_port(GPIOE_BASE);
+		
+		// Enable the ps2 x-dir as input
+		gpio_config_enable_input(GPIOE_BASE, PE2 | PE3);
+	
+		// configure as anolog 
+		gpio_config_analog_enable(GPIOE_BASE, PE2 | PE3);
+		
+		// configure alternate funtion
+		gpio_config_alternate_function(GPIOE_BASE, PE2 | PE3);	
+}
+
+void lcdInit (void)
+{
+		lcd_set_pin_config (SSI0_BASE, GPIOC_BASE, PC7, GPIOA_BASE, PA5);
+}
--- src/main.c
+++ src/main.c
@@ -34,62 +34,14 @@
 #include "TM4C123.h"
 
 extern void serialDebugInit(void);
-
-
-#define   DIR_BTN_BASE    GPIOF_BASE
-#define   PS2_BTN_BASE    GPIOE_BASE
-
-// ADD CODE
-// Define the Bitmask for each button below.  You can create the bit mask
-// by shifting 1 to the left by the pin number
-#define   DIR_BTN_UP      (1 << 1)
-#define   DIR_BTN_DOWN    (1 << 4)
-#define   DIR_BTN_LEFT    (1 << 3)
-#define   DIR_BTN_RIGHT   (1 << 2)
-
-#define   PS2_BTN         (1 << 0)
+extern void lcdInit(void);
+extern void test_lcd(void);
 
 
 /******************************************************************************
  * Global Variables
  *****************************************************************************/
-void print_buttons(void)
-{
-  uint8_t data;
-  uint32_t i;
-  while(1)
-  {
-    data = GPIOF->DATA;
-    
-    if( (data & DIR_BTN_UP) == 0)
-    {
-      printf("BTN_UP pressed\n\r");
-    }
-    
-    if( (data & DIR_BTN_DOWN) == 0)
-    {
-      printf("BTN_DOWN pressed\n\r");
-    }
-    
-    if( (data & DIR_BTN_RIGHT) == 0)
-    {
-      printf("BTN_RIGHT pressed\n\r");
-    }
-    
-    if( (data & DIR_BTN_LEFT) == 0)
-    {
-      printf("BTN_LEFT pressed\n\r");
-    }
-    
-    if((GPIOE->DATA & PS2_BTN) == 0)
-    {
-      printf("PS2_BTN pressed\n\r");
-    }
-    
-    for(i=0;i<100000; i++){}
-    
-  }
-}
+
 
 
 //*****************************************************************************
@@ -101,33 +53,37 @@ void print_buttons(void)
 // Uncomment the source code once you  have implemented the following function
 // void spiTx(uint32_t base, uint8_t *txData, int numBytes, uint8_t *rxData)
 //*****************************************************************************
-//void test_lcd(void)
-//{
-//  int i,j;
-//  
-//  if ( lcd_initialize() == false)
-//  {
-//    printf("\n\r ***** Error initializing LCD *****\n\r");
-//    while(1){};
-//  }
-//   lcd_clear();
-//  for(i = 0; i < 7; i++)
-//  {
-//    
-//     lcd_set_page(i+1);
-//    for(j=0; j<56; j++)
-//    {
-//      lcd_set_column(j+20);
-//      lcd_write_data(motionW[i*56 + j]);
-//    }
-//  }
-//  
-//  for(i=0; i < 25000000; i++);
-//  
-//  lcd_clear();
-//  lcd_write_string_10pts(1, "ECE353");
-//  lcd_write_string_10pts(2, "Project");
-//}
+void test_lcd(void)
+{
+  int i,j;
+  
+  if ( lcd_initialize() == false)
+  {
+    printf("\n\r ***** Error initializing LCD *****\n\r");
+    while(1){};
+  }
+	else
+	{
+			printf("\n\r Initialized");
+	}
+   lcd_clear();
+  for(i = 0; i < 7; i++)
+  {
+    
+     lcd_set_page(i+1);
+    for(j=0; j<56; j++)
+    {
+      lcd_set_column(j+20);
+      lcd_write_data(motionW[i*56 + j]);
+    }
+  }
+  
+  for(i=0; i < 25000000; i++);
+  
+  lcd_clear();
+  lcd_write_string_10pts(1, "ECE353");
+  lcd_write_string_10pts(2, "Project");
+}
 
 //*****************************************************************************
 //*****************************************************************************
@@ -145,44 +101,14 @@ main(void)
   //    cmd_pin_base,
   //    cmd_pin_num
   //  );
-		int pins = DIR_BTN_UP | DIR_BTN_DOWN | DIR_BTN_LEFT | DIR_BTN_RIGHT;
-  serialDebugInit();
-
-  printf("\n\r************************************\n\r");
-  printf("ECE353 - GPIO In Class Exercise\n\r");
-  printf("************************************\n\r");
-  
-  // ADD CODE
-  // Configure the 5 buttuns connected to the pushbuttons on the ECE353 Carrier
-  //  i.	Enable the port used for the pushbuttons
-	gpio_enable_port(DIR_BTN_BASE);
-	gpio_enable_port(PS2_BTN_BASE);
-  // ii.	Configure the 5 pushbuttons as digital pins
-	gpio_config_digital_enable(DIR_BTN_BASE, pins);
-	gpio_config_digital_enable(PS2_BTN_BASE, PS2_BTN);
+	lcdInit();
 	
-  // iii.	Configure the 5 pushbuttons as inputs
-	gpio_config_enable_input(DIR_BTN_BASE, pins);
-	gpio_config_enable_input(PS2_BTN_BASE, PS2_BTN);
-  // iv.	Configure the 5 pushbuttons with internal pull-up resistors
-	gpio_config_enable_pullup(DIR_BTN_BASE, pins);
-	gpio_config_enable_pullup(PS2_BTN_BASE, PS2_BTN);
-	
-  // Use the #defines for DIR_BTN_BASE, DIR_BTN_UP, DIR_BTN_DOWN, 
-  // DIR_BTN_LEFT, DIR_BTN_RIGHT when calling the configuration routines.
-  
-  // Use the #defines for PS2_BTN_BASE and PS2_BTN when calling the configuration routines.
-
-  
-  
-  // Get User Input
-  print_buttons();
 
   // Uncomment the line below only after you  have configured the 
   // SPI and GPIO pins used by the LCD.  If you have configured the
   // interface correctly, the LCD should display an image and a message
   
-  //test_lcd();
+  test_lcd();
   
   // Infinite Loop
   while(1){
